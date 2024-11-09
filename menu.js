142505520da034fa993d71a47f543b7ba6dc6fe2
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2024-11-09 15:11:06 +1000

    Update

---
+++ menu.js
@@ -0,0 +1,15 @@
+var ingredients = {
+    blueberry: Symbol("blueberry"),
+    banana: Symbol("banana"),   
+    strawberry: Symbol("strawberry"),   
+    mango: Symbol("mango"),
+    coconut: Symbol("coconut"),   
+}
+var toppingIngredients = [ingredients.banana,ingredients.coconut]
+
+
+
+
+
+
+
